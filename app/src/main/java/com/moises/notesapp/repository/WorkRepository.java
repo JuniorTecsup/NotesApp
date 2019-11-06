7990fdd3889bdf142564ab19d65cfe852c0d690f
Moises Loarte <moises.loartec@gmail.com>
2019-11-06 17:17:55 -0500

    Initial commit

---
+++ app/src/main/java/com/moises/notesapp/repository/WorkRepository.java
@@ -0,0 +1,13 @@
+package com.moises.notesapp.repository;
+
+import com.moises.notesapp.models.Note;
+
+import java.util.List;
+
+public class WorkRepository {
+
+    List<Note> notas = null;
+
+
+
+}
